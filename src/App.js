828199fd961dff4365eadaad7f5ac4b2f12fdb6e
chantie84 <cc.crompvoets@gmail.com>
2025-04-07 11:59:33 +0200

    Added link to my GitHub repository

--- src/App.js
+++ src/App.js
@@ -9,7 +9,16 @@ function App() {
         <h1>Weather app</h1>
         <WeatherSearch />
       </header>
+      <a
+        href="https://github.com/chantie84/weather-react"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Open-sourced on GitHub
+      </a>
+      ;
     </div>
   );
 }
+
 export default App;
