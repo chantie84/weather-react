1f8436ce79008f163d194b5e2e9549aa32c4eaa4
chantie84 <cc.crompvoets@gmail.com>
2025-04-07 17:02:20 +0200

    added footer

--- src/index.js
+++ src/index.js
@@ -10,11 +10,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
-<footer>
-  <a href="https://github.com/chantie84/weather-react">
-    Link to my Github repository
-  </a>
-</footer>;
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
